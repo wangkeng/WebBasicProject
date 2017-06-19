019a45e40e88143f7f4048577216ec72c3a5b21a
wangkeng <wang6686993@qq.com>
2017-06-19 09:59:01 +0800

    改变包名

--- basic-parent/basic-personInterface/src/main/java/com/dubbo/person/model/UserCategoryQueryModel.java
+++
@@ -1,7 +0,0 @@
-package com.dubbo.person.model;
-
-public class UserCategoryQueryModel extends UserCategory{
-
-	private static final long serialVersionUID = 1L;
-
-}
--- basic-parent/basic-personInterface/src/main/java/com/dubbo/person/model/UserQueryModel.java
+++
@@ -1,7 +0,0 @@
-package com.dubbo.person.model;
-
-public class UserQueryModel extends User{
-
-	private static final long serialVersionUID = 1L;
-
-}
